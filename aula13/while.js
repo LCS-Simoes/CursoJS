b8b9de73c8244c2c51b8c58e94a182e264e5a446
Lucas Simões <lucasbritosimoes@hotmail.com>
2024-07-04 00:27:20 -0300

    Aula 13-14

---
+++ aula13/while.js
@@ -0,0 +1,7 @@
+var c = 1 
+while (c <= 3){
+    console.log(`Passo ${c}`)
+    c++
+}
+//Mesma ideia de C, não tem erro my brother
+//Nome pra While (Estrutura de repetição de dados ao inicio, porém também pode ser no final)
